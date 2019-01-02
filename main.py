30f137473c66a750571707d584421743d3619348
ubuntu-zerk <zerk@ubuntu.com>
2019-01-02 19:50:04 +0800

    modify id

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
+import os
+import re
+
+import pandas as pd
+
+
+def get_num(str):
+    """
+    :param str: str
+    :return: int
+    """
+    return int(re.findall('[0-9]+', str)[0])
+
+
+def sort_by_id(dataframe):
+    """
+    :param dataframe:pd.dataframe
+    :return: pd.data.frame
+    """
+    dataframe['样本id'] = dataframe['样本id'].apply(get_num)
+    return dataframe.sort_values('样本id')
+
+
 if __name__ == '__main__':
-    print('hello')
+    filename = os.path.join(os.getcwd(), 'tc', 'data', 'test.csv')
+    savefile = os.path.join(os.getcwd(), 'tc', 'data', 'new_test.csv')
+    test = pd.read_csv(filename, sep=',',encoding='gbk')
+    data = sort_by_id(dataframe=test)
+    data.to_csv(savefile, index=0)
